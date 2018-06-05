db797d6f00e971ad22b99afd3ca2336bda2ab55a
Michael Smoot <msmoot@syntheticgenomics.com>
2018-06-05 16:35:58 -0700

    Added Affero GPL license

--- sgidspace/architecture.py
+++ sgidspace/architecture.py
@@ -1,3 +1,21 @@
+#------------------------------------------------------------------------------
+#
+#  This file is part of sgidspace.
+#
+#  sgidspace is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  sgidspace is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with sgidspace.  If not, see <http://www.gnu.org/licenses/>.
+#
+#------------------------------------------------------------------------------
 from keras.layers import Input, Conv1D, Dense, MaxPooling1D, Flatten, Activation, Reshape, Subtract
 from keras.layers.normalization import BatchNormalization
 import tensorflow as tf
@@ -110,4 +128,3 @@ def generic_decoder(embedding, output):
         raise ValueError('Output type "' + str(output['type']) + '" is not supported.')
 
     return x
-
--- sgidspace/sgikeras/metrics.py
+++ sgidspace/sgikeras/metrics.py
@@ -1,3 +1,21 @@
+#------------------------------------------------------------------------------
+#
+#  This file is part of sgidspace.
+#
+#  sgidspace is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  sgidspace is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with sgidspace.  If not, see <http://www.gnu.org/licenses/>.
+#
+#------------------------------------------------------------------------------
 from keras import backend as K
 
 def precision(y_true, y_pred):
@@ -58,4 +76,3 @@ def fmeasure(y_true, y_pred):
     Here it is only computed as a batch-wise average, not globally.
     """
     return fbeta_score(y_true, y_pred, beta=1)
-
